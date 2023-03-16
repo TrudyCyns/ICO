661a66946de0f79f8aa916b9f549e5cd2b57bf3d
Trudy <gcyn51a@gmail.com>
2023-03-16 13:45:54 +0300

    Add a file for constants.

---
+++ ico-contract/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xb8b0A81c1D348CcF61259781F1c827AA469bbC6C";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
